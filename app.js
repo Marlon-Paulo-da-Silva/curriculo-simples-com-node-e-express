3b42b7e3c7d54b4cca89d7c28fc214232f8de836
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-23 02:31:54 -0300

    separando arquivos de rotas

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const CurriculoController = require('./controllers/curriculo-controller');
 
 const port = 3000;
 const app = express();
@@ -10,18 +9,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next)=> {
-    res.render('index',{
-        title: "Meu Primeiro servidor Express",
-        version:"0.0.0"
-    });
-});
-
-app.get('/curriculo', (req, res, next) => {
-   const curriculoData = CurriculoController.getData();
-
-   res.render('curriculo', curriculoData);
-});
 
 app.listen(port, err => {
     console.log(`Servidor está ouvindo na porta: ${port}`);
