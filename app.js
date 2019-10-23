fab40d211b3441d13e10dd014826581fcd9f860f
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-23 01:44:48 -0300

    criando pagina e a rota curriculo

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const port = 3000;
 const app = express();
 
+//Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res, next)=> {
-    res.send({
+    res.render('index',{
         title: "Meu Primeiro servidor Express",
         version:"0.0.0"
     });
+});
+
+app.get('/curriculo', (req, res, next) => {
+    res.render('curriculo');
 });
 
 app.listen(port, err => {
