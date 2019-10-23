16142145f08ba64ea608114a2e90e7c5b1be5fb1
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-23 01:59:35 -0300

    alimentando a pagina curriculo com dados do servidor

--- app.js
+++ app.js
@@ -16,7 +16,27 @@ app.get('/', (req, res, next)=> {
 });
 
 app.get('/curriculo', (req, res, next) => {
-    res.render('curriculo');
+    res.render('curriculo',{
+        title: 'Meu Curriculo',
+        name: 'Marlon Paulo da Silva',
+        profession: 'Estagiário',
+        description:'Estagiário da tecnologia, sendo monitor de aulas de realidade virtual e aumentada',
+        experience:[{
+            company: 'Inova Prudente',
+            office:'Estagiário',
+            description: 'Monitor de aulas de RV e RA'
+        },
+        {
+            company: 'Secretaria de Tecnologia de Presidente Prudente',
+            office: 'Estagiário',
+            description: 'Manutenção de computadores'
+        }],
+        education:[{
+            institution: 'Fatec - Presidente Prudente',
+            description: 'Tecnólogo em Análise e Desenvolvimento de Sistemas',
+        }],
+        skills: ['frontend, backend, mobile']
+    });
 });
 
 app.listen(port, err => {
